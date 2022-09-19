47e8da0ef53dc37f08148687c6bc597edd85c146
Mellow1213 <dpcks7282@gmail.com>
2022-09-19 14:50:55 +0900

    Dish.cs 수정

--- Raon_Project_2022/Assets/02. Script/DIsh.cs
+++ Raon_Project_2022/Assets/02. Script/DIsh.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class Dish : MonoBehaviour
 {
-    string[] ingredient = new string[10]; // 접시에 담긴 재료 정보 저장 배열 -> 최대 재료 10개
+    string[] ingredient = new string[5]; // 접시에 담긴 재료 정보 저장 배열 -> 최대 재료 10개
     int ingredientCnt = 0; // 접시에 담긴 재료 개수 저장
 
     float foodHeight;
     FoodType foodtype;
 
+    public int MaximumIngredient = 5;
+
     void Start()
     {
-        foodHeight = 0.1f;
+        foodHeight = 0f;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Food")
         {
-            other.transform.parent = gameObject.transform.parent;
-            foodtype = other.GetComponent<FoodType>();
+            if (ingredientCnt < MaximumIngredient)
+            {
+                other.transform.parent = gameObject.transform.parent;
+                foodtype = other.GetComponent<FoodType>();
+                Vector3 foodPos = new Vector3(0, foodHeight, 0);
+                foodHeight += 0.1f;
+                GameObject temp = Instantiate(foodtype.platingPrefab, transform.position + foodPos, transform.rotation);
+                temp.transform.parent = transform;
+                Destroy(other.gameObject);
+                ingredient[ingredientCnt] = foodtype.food_name;
+                ingredientCnt++;
+                Debug.Log("음식의 갯수 = " + ingredientCnt);
+            }
+            else
+                Debug.Log("재료를 10개 이상 담을 수 없습니다.");
+        }
+    }
 
-            Vector3 foodPos = new Vector3(0, foodHeight, 0);
-            foodHeight += 0.1f;
-            GameObject temp = Instantiate(foodtype.platingPrefab, transform.position + foodPos, transform.rotation);
-            temp.transform.parent = transform;
-            Destroy(other.gameObject);
-            ingredientCnt++;
 
-            Debug.Log("음식의 갯수 = " + ingredientCnt);
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("접시에 있는 음식들 : ");
+            for (int i = 0; i < ingredientCnt; i++)
+            {
+                Debug.Log("\n" + ingredient[i]);
+            }
         }
     }
 }
