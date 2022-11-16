a64b56ea365e892eddc5c6fae3e15b5b3d2445d6
Mellow1213 <dpcks7282@gmail.com>
2022-11-16 18:40:55 +0900

    재료 뽑을 때 마다 돈 감소

--- Raon_Project_2022/Assets/02. Script/Food/FoodDispenser.cs
+++ Raon_Project_2022/Assets/02. Script/Food/FoodDispenser.cs
@@ -6,8 +6,10 @@ public class FoodDispenser : MonoBehaviour
 {
     [SerializeField] private GameObject foodPrefab;
     public Transform spawnPos;
+    public int cost;
     public void SpawnPrefab()
     {
         Instantiate(foodPrefab, spawnPos.transform.position, spawnPos.transform.rotation);
+        GameManager.instance.myGold -= cost;
     }
 }
