fc7de54e3ef36ad07f2371dd409e2ce84c74636e
Mellow1213 <dpcks7282@gmail.com>
2022-11-21 12:41:52 +0900

    자리 배열 초기화

--- Raon_Project_2022/Assets/PosManager.cs
+++ Raon_Project_2022/Assets/PosManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PosManager : MonoBehaviour
 {
     public Transform[] positions;
+
+    bool[] seat;
+
+    private void Start()
+    { 
+        seat = Enumerable.Repeat<bool>(false, positions.Length).ToArray<bool>(); 
+    }
+
 
 }
