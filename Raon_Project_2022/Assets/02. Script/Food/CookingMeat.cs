acc553ebe1ba894ede3222d68dc80cfd64896442
Mellow1213 <dpcks7282@gmail.com>
2022-11-22 23:09:45 +0900

    햄버거 굽는 소리 및 스크립트 추가

--- Raon_Project_2022/Assets/02. Script/Food/CookingMeat.cs
+++ Raon_Project_2022/Assets/02. Script/Food/CookingMeat.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CookingMeat : MonoBehaviour
 {
+    AudioSource _audioSource;
     public GameObject cookedPrefab;
     float timer = 0;
     public bool isCooking = false;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
         Debug.Log("timer = " + timer);
         if (isCooking)
+        {
             timer += Time.deltaTime;
+            if(!_audioSource.isPlaying)
+                _audioSource.Play();
+        }
+        else
+        {
+            if (_audioSource.isPlaying)
+            {
+                _audioSource.Pause();
+            }
+        }
 
         if (timer > 10)
         {
@@ -25,6 +42,8 @@ public class CookingMeat : MonoBehaviour
     private void OnCollisionStay(Collision collision)
     {
         if (collision.collider.CompareTag("Grill"))
+        {
             isCooking = true;
+        }
     }
 }
